ac61090cdf73871b8602c6c54be04c2c12e905da
Admin <kaWork89@gmail.com>
2023-08-30 16:48:51 +0300

    first step

--- src/Lesson7/Main.java
+++ src/Lesson7/Main.java
@@ -6,15 +6,30 @@ import static Lesson7.Zoo.AnimalArray;
 
 public class Main {
     public static void main(String[] args) {
-
-Zoo Zoo1=new Zoo(50);
-Zoo1.setAnimalArray();
-
-Zoo1.feadAnimals();
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        Zoo Zoo1 = new Zoo(50);
+        Zoo1.setAnimalArray();
+        int x;
+        do {
+            System.out.println("Enter numbers");
+            System.out.println("Enter 1 to show ALL ANIMALS IN ZOO");
+            System.out.println("Enter 2 to feed ANIMALS");
+            System.out.println("Enter 0 to EXIT");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Enter number");
+                scanner.next();
+            }
+            x = scanner.nextInt();
+            switch (x) {
+                case 1:
+                    Zoo1.showAnimals();
+                    break;
+                case 2:
+                    Zoo1.feadAnimals();
+                    break;
+                default:
+            }
         }
-
-
+        while (x != 0);
     }
+}
--- src/Lesson7/Zoo.java
+++ src/Lesson7/Zoo.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Zoo {
     static int cellAmount;
-    static Animal[] AnimalArray;
+    static Animal[] AnimalArray = new Animal[50];
 
     public Zoo(int cellAmount) {
         this.cellAmount = cellAmount;
@@ -13,22 +13,10 @@ public class Zoo {
     Scanner scanner = new Scanner(System.in);
 
     public void setAnimalArray() {
-
-        System.out.println("Enter animals");               ////////// enter Animals
-        int counter = 10;
-        int idAnimal = 0;
-        while (counter != 0) {
-            System.out.print("1 for Parrot   ");
-            System.out.print(" 2 for Eagle    ");
-            System.out.print("3 for Tiger    ");
-            System.out.print("4 for Wolf     ");
-            System.out.print("5 for Elephant   ");
-            System.out.print("6 for Antiloppe   ");
-            System.out.print("7 for Shark     ");
-            System.out.print("8 for Carp    ");
-            System.out.print("0 for EXIT    ");
-            counter = scanner.nextInt();
-
+        int counter;
+                       ////////// enter Animals
+        for (int idAnimal = 0; idAnimal < AnimalArray.length; idAnimal++) {
+            counter = (int) (Math.random() * 8 + 1);
             switch (counter) {
                 case 1:
                     AnimalArray[idAnimal] = new Parrot("Parrot", idAnimal, "cellSize", 15, "Gosha", 25);
@@ -37,42 +25,41 @@ public class Zoo {
                     AnimalArray[idAnimal] = new Eagle("Eagle", idAnimal, "cellSize", 15, 25);
                     break;
                 case 3:
-                    AnimalArray[idAnimal] = new Tiger(idAnimal,"Tiger",  "cellSize", 15);
+                    AnimalArray[idAnimal] = new Tiger(idAnimal, "Tiger", "cellSize", 15);
                     break;
                 case 4:
-                    AnimalArray[idAnimal] = new Wolf(idAnimal,"Wolf", "cellSize", "Leader");
+                    AnimalArray[idAnimal] = new Wolf(idAnimal, "Wolf", "cellSize", "Leader");
                     break;
                 case 5:
-                    AnimalArray[idAnimal] = new Elephant(idAnimal,"Elephant", 1000, 5);
+                    AnimalArray[idAnimal] = new Elephant(idAnimal, "Elephant", 1000, 5);
                     break;
                 case 6:
-                    AnimalArray[idAnimal] = new Antiloppe(idAnimal,"Antiloppe", 80, 89);
+                    AnimalArray[idAnimal] = new Antiloppe(idAnimal, "Antiloppe", 80, 89);
                     break;
                 case 7:
-                    AnimalArray[idAnimal] = new Shark(idAnimal,"Shark", "sault water", 15);
+                    AnimalArray[idAnimal] = new Shark(idAnimal, "Shark", "sault water", 15);
                     break;
                 case 8:
-                    AnimalArray[idAnimal] = new Carp(idAnimal,"Carp", "fresh water", 15);
+                    AnimalArray[idAnimal] = new Carp(idAnimal, "Carp", "fresh water", 15);
                     break;
                 default:
             }
-
-            idAnimal++;
-
         }
+
+
     }
 
-        ///////////////////////////////////////// feed Animals
- public void feadAnimals(){
-   //  AnimalArray[0].getAnimalAmount();
-        System.out.println("LETS feed Animals");
-        int feedCounter=10;
-        while(feedCounter!=0){
-            System.out.println("What Animal do you want to feed enter id ");
-            int idAnimal= scanner.nextInt();
-          //  AnimalArray[idAnimal].getAnimalAmount();
-            System.out.println(AnimalArray[idAnimal].getAnimalType());
 
+    ///////////////////////////////////////// feed Animals
+    public void feadAnimals() {
+        //  AnimalArray[0].getAnimalAmount();
+        System.out.println("LETS feed Animals");
+        int feedCounter = 10;
+        while (feedCounter != 0) {
+            System.out.println("What Animal do you want to feed enter id ");
+            int idAnimal = scanner.nextInt();
+            //  AnimalArray[idAnimal].getAnimalAmount();
+            System.out.println(AnimalArray[idAnimal].getAnimalType());
 
 
             System.out.print("1 for fresh meat   ");
@@ -83,7 +70,7 @@ public class Zoo {
             System.out.print("0 for EXIT    ");
             feedCounter = scanner.nextInt();
 
-            switch (feedCounter){
+            switch (feedCounter) {
                 case 1:
                     AnimalArray[idAnimal].feedAnimal(1);
                     break;
@@ -99,13 +86,21 @@ public class Zoo {
                 case 5:
                     AnimalArray[idAnimal].feedAnimal(5);
                     break;
+                default:
+                    System.out.println("NO food type");
             }
 
         }
 
 
-
     }
 
+    public void showAnimals() {
+        for (int i = 0; i < AnimalArray.length; i++) {
+            System.out.print(AnimalArray[i].getAnimalType() + " ");
+            if (i % 10 == 0)
+                System.out.println();
+        }
+    }
 
 }
